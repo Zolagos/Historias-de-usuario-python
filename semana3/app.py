01189ed1d954b87e30f3c93cddbd6a971e62a53c
Sharon Meriño <smmerino@outlook.com>
2026-04-01 22:37:49 -0500

    cm

--- semana3/app.py
+++ semana3/app.py
@@ -1,10 +1,64 @@
 #nombre": str, "precio": float, "cantidad": int}
 from servicios import *
 
-inventario = {1:{"nombre":"lapiz", "precio":2000, "cantidad":2}}
-nombre = input("Digite el nombre del producto: ")
-precio = float(input("Digite el precio del producto: "))
-cantidad = int(input("Digite la cantidad del producto: "))
 
-agregar_producto(inventario, nombre, precio, cantidad)
-print(inventario)
+
+inventario =[]
+opcion = ""
+while opcion != "9":
+    print("=" * 40)
+    print(" 📦 SISTEMA DE GESTIÓN DE INVENTARIO 📦")
+    print("=" * 40)
+    print("  [1] Agregar producto")
+    print("  [2] Mostrar inventario")
+    print("  [3] Buscar producto")
+    print("  [4] Actualizar producto")
+    print("  [5] Eliminar producto")
+    print("  [6] Estadísticas del inventario")
+    print("  [7] Guardar datos en CSV")
+    print("  [8] Cargar datos desde CSV")
+    print("  [9] Salir")
+    print("=" * 40) 
+    opcion = input("👉 Seleccione una opción (1-9): ")
+    if opcion == "1":
+        try:
+            nombre = input("Digite el nombre del producto: ")
+            precio = float(input("Digite el precio del producto: "))
+            if precio <= 0:
+                raise ValueError
+            cantidad = int(input("Digite la cantidad del producto: "))
+            if precio <= 0:
+                raise ValueError
+            agregar_producto(inventario, nombre, precio, cantidad)
+        except ValueError:
+            print("Dato invalido")
+    if opcion == "2":
+        mostrar_inventario(inventario)
+    if opcion == "3":
+        nombre = input("Digite el nombre del producto: ")
+        producto = buscar_producto(inventario, nombre)
+        if producto:
+                print(f"\n✅ Encontrado: {producto['nombre']} | ${producto['precio']} | Stock: {producto['cantidad']}")
+        else:
+                print("\n❌ Producto no encontrado.")
+    if opcion == "4":
+        try:
+            nombre = input("Digite el nombre del producto a actualizar: ")
+            precio = float(input("Digite el nuevo precio del producto: "))
+            if precio <= 0:
+                raise ValueError
+            cantidad = int(input("Digite la nueva cantidad del producto: "))
+            if precio <= 0:
+                raise ValueError
+            actualizar_producto(inventario, nombre, nuevo_precio=precio, nueva_cantidad=cantidad)
+        except ValueError:
+            print("Dato invalido")
+    if opcion == "5":
+        nombre = input("Digite el nombre del producto: ")
+        eliminar_producto(inventario, nombre)
+    if opcion == "6":
+        calcular_estadisticas(inventario)
+
+
+
+
--- semana3/servicios.py
+++ semana3/servicios.py
@@ -7,17 +7,111 @@ eliminar_producto(inventario, nombre)
 calcular_estadisticas(inventario) → retorna tupla/dict con métricas
 """
 
-cont = 1
 
-def agregar_producto(inventario, nombre, precio, cantidad):
-    global cont
-    cont+=1
-    inventario[cont]={"nombre":nombre, "precio":precio, "cantidad":cantidad}
+def agregar_producto(inventario: list[dict[str,float|str|int]], nombre:str, precio:float, cantidad:int):
+    """
+    La funcion agrega productos al inventario
+    Inventario: list 
+    nombre: str
+    precio: float
+    cantidad: int
 
-def mostrar_inventario(inventario):
-    print(inventario)
-    #buscar para que se vea mejor
+    """
+    nuevo_producto ={
+        "nombre":nombre, 
+        "precio":precio, 
+        "cantidad":cantidad
+        }
+    inventario.append(nuevo_producto)
+    print(f"\n✅ Producto '{nombre}' agregado correctamente.")
 
-def buscar_producto(inventario, nombre):
-    for llave, clave in inventario.items():
-        if clave.get
+def mostrar_inventario(inventario: list[dict[str,float|str|int]]):
+    """
+    La funcion muestra el inventario actual
+    inventario: list
+
+    """
+    if not inventario:
+        print("\n[!] El invemtario esta vacio.")
+        return
+    print("\n" + "=" * 50)
+    print(f" {'Nombre del producto':<20} | {'Precio':>10} | {'Cant.':>6}")
+    print("=" * 50)
+
+    for datos in inventario:
+        nombre = datos["nombre"]
+        precio = datos["precio"]
+        cantidad = datos["cantidad"]
+        print(f" {nombre:<20} | {precio:>10.2f} | {cantidad:>6}")
+    print("=" * 50 + "\n")
+
+
+def buscar_producto(inventario: list[dict[str,float|str|int]], nombre:str) -> dict|None:
+    """
+    Inventario: list 
+    nombre: str
+
+    La funcion retorna el producto encontrado
+    return:
+    
+    dict | none
+    """
+
+    for producto in inventario:
+        if producto ['nombre'] == nombre:
+            return producto
+    return  None
+        
+
+def actualizar_producto(inventario: list[dict[str,float|str|int]], nombre:str, nuevo_precio=None, nueva_cantidad=None):
+    producto = buscar_producto(inventario,nombre)
+    if producto:
+        producto['precio'] = nuevo_precio
+        producto['cantidad'] = nueva_cantidad
+        print(f"🔄 '{nombre}' actualizado.")
+    else:
+        print("❌ Error: Producto no encontrado.")
+
+def eliminar_producto(inventario: list[dict[str,float|str|int]], nombre:str):
+    producto = buscar_producto(inventario,nombre)
+    if producto:
+        inventario.remove(producto)
+        print(f"🗑️ '{nombre}' ha sido eliminado.")
+    else:
+        print("❌ Error: No se pudo eliminar el producto.")
+
+def calcular_estadisticas(inventario:list[dict[str,float|str|int]]):
+    suma_cantidad = 0
+    suma_valor_total = 0
+    producto_mas_caro = 0
+    producto_mayor_stock = 0
+    nombre_producto_mas_caro = ""
+    nombre_producto_mayor_stock = ""
+    for producto in inventario:
+        cantidad = producto['cantidad']
+        precio = producto['precio']
+        nombre = producto['nombre']
+        suma_cantidad += cantidad
+        valor_total = precio * cantidad
+        suma_valor_total += valor_total
+        if precio > producto_mas_caro:
+            nombre_producto_mas_caro = nombre
+            producto_mas_caro = precio
+        if cantidad > producto_mayor_stock:
+            nombre_producto_mayor_stock = nombre
+            producto_mayor_stock = cantidad
+
+    estadistica = {
+        "suma cantidad":suma_cantidad,
+        'valor total':suma_valor_total,
+        'producto mas caro':(nombre_producto_mas_caro,producto_mas_caro),
+        'producto con mayor stock':(nombre_producto_mayor_stock,producto_mayor_stock)
+    }
+
+    print(f" {'Estadistica':<20} | {'Valor'}")
+    print("=" * 40)
+    for est, valor in estadistica.items():
+        print(f" {est:<20} | {valor}")
+    print("=" * 40 + "\n")
+
+
